c050cd644daa8c2b171e9fcc4a66c29706d371de
PatPrograming <30586247+PatPrograming@users.noreply.github.com>
2020-12-05 11:34:34 -0800

    :slot_machine:

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,8 +1,17 @@
 package arrays;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+public static void main(String[] args) {
+	 Robot [] robots = new Robot[5];
+	 for (int i = 0; i < robots.length; i++) {
+		robots[i]=new Robot();
+		robots[i].setX(50*i);
+		robots[i].setY(0);
+	}
+}
 	//2. create an array of 5 robots.
 
 	//3. use a for loop to initialize the robots.
